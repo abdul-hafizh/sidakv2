30697091d47ca0b3a0a7a88147bf6ce45bfb8034
yosef kely <yosef.kely89@gmail.com>
2023-07-27 22:20:32 +0700

    ini testing

--- app/Http/Controllers/LayoutController.php
+++ app/Http/Controllers/LayoutController.php
@@ -1,34 +1,35 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use App\Helpers\ConfigHeader;
 use App\Helpers\GeneralPaginate;
 
 class LayoutController extends Controller
 {
-   
+
     public function __construct()
     {
         $this->title = ConfigHeader::index();
         $this->template = GeneralPaginate::template();
-       
     }
-    
+
     public function index(Request $request)
     {
-        return view('template/'.$this->template.'.index')->with(['title'=>$this->title]); 
+        return view('template/' . $this->template . '.index')->with(['title' => $this->title]);
+    }
+
+    public function test(Request $request)
+    {
+        return view('template/' . $this->template . '.index')->with(['title' => $this->title]);
     }
 
 
 
-    public function show(Request $request){
+    public function show(Request $request)
+    {
 
-        return view('template/'.$this->template.'.index')->with(['title'=>$this->title]); 
+        return view('template/' . $this->template . '.index')->with(['title' => $this->title]);
     }
-
-     
-
-    
-    
 }
