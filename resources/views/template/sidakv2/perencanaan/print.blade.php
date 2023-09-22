a85abdf6114085e476bca9462d209f5fc027ba05
Abdul Hafizh <abdulha05518@gmail.com>
2023-09-22 19:20:29 +0700

    route

--- resources/views/template/sidakv2/perencanaan/print.blade.php
+++ resources/views/template/sidakv2/perencanaan/print.blade.php
@@ -1,34 +1,4 @@
-<div style="display: none">
-  <div style="margin: 0px 20px;" >
-    <div style="text-align:center;padding: 20px 0px 20px;">
-      <h4 style="font-size: 14px;">LAPORAN RENCANA PENGGUNAAN DAK NONFISIK FASILITASI PENANAMAN MODAL </h4>
-      <h4 style="font-size: 14px;">DINAS PENANAMAN MODAL DAN PELAYANAN TERPADU SATU PINTU </h4>
-      <h4 style="font-size: 14px;text-transform: uppercase;">--  daerah_nama  --</h4>
-      <h4 style="font-size: 14px;">TAHUN ANGGARAN --  periode_tahun  --</h4>
-    </div>
-
-    <table border="0" id="myTable" style="font-size: 13px;margin: 0px 20px;">
-      <thead style="border-top: 1px solid #000;text-align: center;">
-        <tr>
-          <th rowspan="2" style="text-align: center;padding: 5px 10px;border-left: 1px solid #000;border-bottom: 1px solid #000;">No</th>
-          <th rowspan="2" colspan="2" style="text-align: center;padding: 5px 10px;border-left: 1px solid #000;border-bottom: 1px solid #000;">Kegiatan/Sub Kegiatan</th>
-          <th colspan="2" style="text-align: center;padding: 5px 10px 10px;border-left: 1px solid #000;border-bottom: 1px solid #000;">Perencanaan</th>
-          <th rowspan="2" colspan="2" style="text-align: center;padding: 5px 0px;border-left: 1px solid #000;border-right: 1px solid #000;border-bottom: 1px solid #000;">Pagu APBN (Rp)</th>
-        </tr>
-        <tr style="text-align: center;padding: 5px 10px;border-right: 1px solid #000;border-left: 1px solid #000;border-bottom: 1px solid #000;">
-          <th style="text-align: center;padding: 5px 10px;border-left: 1px solid #000;border-bottom: 1px solid #000;">Jumlah</th>
-          <th style="text-align: center;padding: 5px 10px;border-left: 1px solid #000;border-bottom: 1px solid #000;">Satuan</th>
-        </tr>
-      </thead>
-      <tbody id="exportView">
-        
-      </tbody>
-    </table>
-  </div>
-</div>
-
-
-<div style="display: none">
+<div id="dataPrPdf">
   <div style="margin: 0px 20px;">
     <div style="text-align:center;padding: 20px 0px 20px;">
       <h4 style="font-size: 14px;">LAPORAN RENCANA PENGGUNAAN DAK NONFISIK FASILITASI PENANAMAN MODAL </h4>
@@ -36,7 +6,7 @@
       <h4 style="font-size: 14px;text-transform: uppercase;">--  daerah_nama  --</h4>
       <h4 style="font-size: 14px;">TAHUN ANGGARAN --  periode_tahun  --</h4>
     </div>
-    <table border="0" id="dataPrPdf" style="font-size: 13px;margin: 0px 20px;">
+    <table border="0" style="font-size: 13px;margin: 0px 20px;">
       <thead style="border-top: 1px solid #000;text-align: center;">
         <tr>
           <th rowspan="2" style="text-align: center;padding: 5px 10px;border-left: 1px solid #000;border-bottom: 1px solid #000;">No</th>
