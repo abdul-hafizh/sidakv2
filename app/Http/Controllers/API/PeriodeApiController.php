68c6d50332e19a7d6b0929c7b936b4197beb6fe0
RIZAL FAOZI <ryzal.kazama@gmail.com>
2023-11-24 09:56:10 +0700

    updatePeriode

--- app/Http/Controllers/API/PeriodeApiController.php
+++ app/Http/Controllers/API/PeriodeApiController.php
@@ -48,13 +48,13 @@ class PeriodeApiController extends Controller
 
             if ($request->action == 'perencanaan')
             {
+                 $query->select('a.id', 'a.slug', 'a.year', 'c.pagu_apbn', 'c.pagu_promosi', 'c.target_pengawasan', 'c.target_bimbingan_teknis', 'c.target_penyelesaian_permasalahan','d.promosi_pengadaan_pagu','d.pengawas_analisa_target','d.pengawas_inspeksi_target','d.pengawas_evaluasi_target','d.bimtek_perizinan_target','d.bimtek_pengawasan_target','d.penyelesaian_identifikasi_target','d.penyelesaian_realisasi_target','d.penyelesaian_evaluasi_target'); 
                  
+                 $query->join('pagu_target as c', 'a.year', '=', 'c.periode_id');
+                 $query->where('c.daerah_id', Auth::User()->daerah_id);
 
-               $query->select('a.id', 'a.slug', 'a.year', 'c.pagu_apbn', 'c.pagu_promosi', 'c.target_pengawasan', 'c.target_bimbingan_teknis', 'c.target_penyelesaian_permasalahan','d.promosi_pengadaan_pagu','d.pengawas_analisa_target','d.pengawas_inspeksi_target','d.pengawas_evaluasi_target','d.bimtek_perizinan_target','d.bimtek_pengawasan_target','d.penyelesaian_identifikasi_target','d.penyelesaian_realisasi_target','d.penyelesaian_evaluasi_target'); 
-               $query->where('d.daerah_id', Auth::User()->daerah_id);
-               $query->join('pagu_target as c', 'a.year', '=', 'c.periode_id');
-               $query->join('perencanaan as d', 'c.periode_id', '=', 'd.periode_id');
-               
+
+        
 
 
                  if($request->type == 'POST')
